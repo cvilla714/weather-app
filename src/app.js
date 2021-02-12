6a17c1d78dea871154b86317fde3f45f997679ed
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-12 00:04:02 -0500

    cashed the DOM

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
