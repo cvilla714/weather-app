f36b6ddbd0d29c726d61351fe0fb59167cb6916e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-13 16:14:53 -0500

    adde the functions to update the units and show between Celciu and Farenheits

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-import { getWeather, getCity } from './forecast';
+import { getWeather, getCity } from "./forecast";
 
-const cityForm = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+const unitToggle = document.querySelector("[data-unit-toggle]");
+const metricRadio = document.getElementById("cel");
+const imperialRadio = document.getElementById("fah");
 
 const updateUI = (data) => {
-  // console.log(data);
+  console.log(data);
   const { cityDetails, weatherDetails } = data;
 
   details.innerHTML = `
@@ -15,23 +18,49 @@ const updateUI = (data) => {
       <h2 class="my-3 zone">Time Zone: ${cityDetails.TimeZone.Code}</h2>
       <div class="my-3 text-info">Forecast: ${weatherDetails.WeatherText}</div>
       <div class="my-4 info">
-          <span >Temperature: ${weatherDetails.Temperature.Metric.Value}</span>
-          <span>&deg;F</span>
-          /
           <span>${weatherDetails.Temperature.Imperial.Value}</span>
-          <span>&deg;C</span>
+          &deg;<span data-temp-unit></span>
       </div>
       `;
 
   const iconSrc = `img/icons/${weatherDetails.WeatherIcon}.svg`;
-  icon.setAttribute('src', iconSrc);
-  const timeSrc = weatherDetails.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-  time.setAttribute('src', timeSrc);
+  icon.setAttribute("src", iconSrc);
+  const timeSrc = weatherDetails.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
 
-  if (card.classList.contains('d-none')) {
-    card.classList.remove('d-none');
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
   }
 };
+
+unitToggle.addEventListener("click", (e) => {
+  console.log(e);
+  let metricUnits = !isMetric();
+  // let metricUnits = !metricRadio.checked;
+  metricRadio.checked = metricUnits;
+  imperialRadio.checked = !metricUnits;
+  updateUnits();
+});
+
+function updateUnits() {
+  const tempUnits = document.querySelector("[data-temp-unit]");
+  console.log(tempUnits);
+  tempUnits.innerText = isMetric() ? "C" : "F";
+}
+
+function isMetric() {
+  return metricRadio.checked;
+}
+
+metricRadio.addEventListener("change", () => {
+  updateUnits();
+});
+
+imperialRadio.addEventListener("change", () => {
+  updateUnits();
+});
+
+function displayTemperature(temperature) {}
 
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
@@ -42,18 +71,18 @@ const updateCity = async (city) => {
   };
 };
 
-cityForm.addEventListener('submit', (event) => {
+cityForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const city = cityForm.city.value.trim();
   cityForm.reset();
   updateCity(city)
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
-  localStorage.setItem('ultimaciudad', city);
+  localStorage.setItem("ultimaciudad", city);
 });
 
-if (localStorage.getItem('ultimaciudad')) {
-  updateCity(localStorage.getItem('ultimaciudad'))
+if (localStorage.getItem("ultimaciudad")) {
+  updateCity(localStorage.getItem("ultimaciudad"))
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 }
