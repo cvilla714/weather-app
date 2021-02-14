155ca7e949bb8deba931dd5abb3555aa170c15b1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-13 22:38:33 -0500

    new content after running build script and lintes

--- src/forecast.js
+++ src/forecast.js
@@ -1,7 +1,7 @@
-const key = "Fa1tORczSKnflphuiPFCKU303Z04wWn9";
+const key = 'Fa1tORczSKnflphuiPFCKU303Z04wWn9';
 
 const getWeather = async (locationid) => {
-  const baseWeather = "https://dataservice.accuweather.com/currentconditions/v1/";
+  const baseWeather = 'https://dataservice.accuweather.com/currentconditions/v1/';
   const queryWeather = `${locationid}?apikey=${key}`;
   const responseWeather = await fetch(baseWeather + queryWeather);
   const dataWeather = await responseWeather.json();
@@ -9,7 +9,7 @@ const getWeather = async (locationid) => {
 };
 
 const getCity = async (city) => {
-  const baseCity = "https://dataservice.accuweather.com/locations/v1/cities/search";
+  const baseCity = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   const queryCity = `?apikey=${key}&q=${city}`;
   const responseCity = await fetch(baseCity + queryCity);
   const dataCity = await responseCity.json();
