f2725f5e874fc1ff925921a7dae07d5e208acaae
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-13 22:45:31 -0500

    last check for linter errors

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 /* eslint-disable eqeqeq */
-import { getWeather, getCity } from './forecast';
+import { getWeather, getCity } from "./forecast";
 
-const cityForm = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
-const unitToggle = document.querySelector('[data-unit-toggle]');
-const metricRadio = document.getElementById('cel');
-const imperialRadio = document.getElementById('fah');
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+const unitToggle = document.querySelector("[data-unit-toggle]");
+const metricRadio = document.getElementById("cel");
+const imperialRadio = document.getElementById("fah");
 
 const updateUI = (data) => {
   // console.log(data);
   const { cityDetails, weatherDetails } = data;
   const maintem = weatherDetails.Temperature.Imperial.Value;
-  const thetime = new Date(weatherDetails.EpochTime).toLocaleTimeString('en-US');
+  const thetime = new Date(weatherDetails.EpochTime).toLocaleTimeString("en-US");
   details.innerHTML = `
       <h5 class="my-3">City Name: ${cityDetails.EnglishName}</h5>
       <h2 class="my-3 zone">Time Zone: ${cityDetails.TimeZone.Code}</h2>
@@ -27,30 +27,30 @@ const updateUI = (data) => {
       `;
 
   const iconSrc = `img/icons/${weatherDetails.WeatherIcon}.svg`;
-  icon.setAttribute('src', iconSrc);
-  const timeSrc = weatherDetails.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-  time.setAttribute('src', timeSrc);
+  icon.setAttribute("src", iconSrc);
+  const timeSrc = weatherDetails.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
 
-  if (card.classList.contains('d-none')) {
-    card.classList.remove('d-none');
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
   }
 
   function convertTemp() {
-    const s = document.querySelector('[data-temp-unit]');
-    const c = document.querySelector('[data-celcius-temp]');
+    const s = document.querySelector("[data-temp-unit]");
+    const c = document.querySelector("[data-celcius-temp]");
     // console.log(s.innerHTML);
     // let returnTemp = maintem;
 
     const returnTemp = Math.round((maintem - 32) * (5 / 9));
     // console.log(returnTemp);
 
-    c.innerHTML = s.innerHTML == 'F' ? maintem : returnTemp;
+    c.innerHTML = s.innerHTML == "F" ? maintem : returnTemp;
     // console.log(maintem);
     return returnTemp;
   }
   convertTemp();
 
-  unitToggle.addEventListener('click', () => {
+  unitToggle.addEventListener("click", () => {
     convertTemp();
   });
 };
@@ -60,11 +60,11 @@ function isMetric() {
 }
 
 function updateUnits() {
-  const tempUnits = document.querySelector('[data-temp-unit]');
+  const tempUnits = document.querySelector("[data-temp-unit]");
   // console.log(tempUnits);
-  tempUnits.innerHTML = isMetric() ? 'C' : 'F';
+  tempUnits.innerHTML = isMetric() ? "C" : "F";
 }
-unitToggle.addEventListener('click', () => {
+unitToggle.addEventListener("click", () => {
   // console.log(e);
   const metricUnits = !isMetric();
   // let metricUnits = !metricRadio.checked;
@@ -74,11 +74,11 @@ unitToggle.addEventListener('click', () => {
   updateUnits();
 });
 
-metricRadio.addEventListener('change', () => {
+metricRadio.addEventListener("change", () => {
   updateUnits();
 });
 
-imperialRadio.addEventListener('change', () => {
+imperialRadio.addEventListener("change", () => {
   updateUnits();
 });
 
@@ -91,18 +91,18 @@ const updateCity = async (city) => {
   };
 };
 
-cityForm.addEventListener('submit', (event) => {
+cityForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const city = cityForm.city.value.trim();
   cityForm.reset();
   updateCity(city)
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
-  localStorage.setItem('ultimaciudad', city);
+  localStorage.setItem("ultimaciudad", city);
 });
 
-if (localStorage.getItem('ultimaciudad')) {
-  updateCity(localStorage.getItem('ultimaciudad'))
+if (localStorage.getItem("ultimaciudad")) {
+  updateCity(localStorage.getItem("ultimaciudad"))
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 }
